7c25b14090af177c58a4e2e3f4fbeeb553568383
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-02-20 15:47:50 +0000

    Update satellite_functions.py
    new general get_landsat_bands function that handles the differentiation of L7, L8, and L9 outside of the IPMLS main body
    removed get_l7_bands(), get_l8_bands(), get_l9_bands()

--- satellite_functions.py
+++ satellite_functions.py
@@ -1,23 +1,15 @@
-def get_l7_bands():
-    BLUE_BAND = '1'
-    GREEN_BAND = '2'
-    NIR_BAND = '4'
-    SWIR1_BAND = '5'
-    MIR_BAND = '7'
-    return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, MIR_BAND
-
-def get_l8_bands():
-    BLUE_BAND = '2'
-    GREEN_BAND = '3'
-    NIR_BAND = '5'
-    SWIR1_BAND = '6'
-    SWIR2_BAND = '7'
-    return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, SWIR2_BAND
-
-def get_l9_bands():
-    BLUE_BAND = '2'
-    GREEN_BAND = '3'
-    NIR_BAND = '5'
-    SWIR1_BAND = '6'
-    SWIR2_BAND = '7'
-    return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, SWIR2_BAND
+def get_landsat_bands(landsat_n):
+    if landsat_n == 7:
+        BLUE_BAND = '1'
+        GREEN_BAND = '2'
+        NIR_BAND = '4'
+        SWIR1_BAND = '5'
+        MIR_BAND = '7'
+        return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, MIR_BAND
+    else:
+        BLUE_BAND = '2'
+        GREEN_BAND = '3'
+        NIR_BAND = '5'
+        SWIR1_BAND = '6'
+        SWIR2_BAND = '7'
+        return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, SWIR2_BAND
