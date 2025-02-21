226038a04dfb22d6da9cea3079f054f477be2f3b
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-02-21 14:12:45 +0000

    Update IPMLS_UNI.py
    added composite and cloud mask functions
    added a '\' to the home folder name
    changed show_landsat to do_landsat
    corrected spelling of 'processing'
    changed landsat bands collection to be handling the if statement in the satellite function
    removed spaced between cleaning indices lines

--- IPMLS_UNI.py
+++ IPMLS_UNI.py
@@ -22,6 +22,12 @@
 - compositing (nothing yet)
 - separating general water and reservoir water (nothing yet)
 """
+# =============================================================================
+# - to install a conda library
+#     - %UserProfile%\miniconda3\condabin\activate
+#     - conda activate ee
+#     - conda install WhateverLibraryYouWant
+# =============================================================================
 # %% Start
 # %%% External Library Imports
 import time
@@ -31,9 +37,9 @@ import os
 import numpy as np
 
 # %%% Internal Function Imports
-from image_functions import compress_images, plot_image
+from image_functions import compress_image, plot_image, composite, cloud_mask
 from calculation_functions import get_indices
-from satellite_functions import get_l7_bands, get_l8_bands, get_l9_bands
+from satellite_functions import get_landsat_bands
 # %% General Landsat Function
 do_l7 = False
 do_l8 = False
@@ -41,10 +47,10 @@ do_l9 = True
 save_images = False
 
 # main parent path where all image files are stored
-home = 'P:\Individual Project'
-compression = 10  # 1 for full-sized images, bigger integer for smaller images
-dpi = 4000 # 3000 for full resolution, below 1000, images become fuzzy
-plot_size = (6, 6) # larger plots increase detail and pixel count
+home = 'P:\\Individual Project'
+compression = 30 # 1 for full-sized images, bigger integer for smaller images
+dpi = 1000 # 3000 for full resolution, below 1000, images become fuzzy
+plot_size = (3, 3) # larger plots increase detail and pixel count
 
 print('compression factor:', compression)
 print('dots per inch (dpi):', dpi)
@@ -53,7 +59,7 @@ print(f'computing indices for landsat 7: {do_l7}, '
       f'landsat 9: {do_l9}')
 print(f'saving images: {save_images}')
 
-def get_landsat(landsat_number, folder, show_landsat):
+def get_landsat(landsat_number, folder, do_landsat):
     print('===================')
     print(f'||LANDSAT {landsat_number} START||')
     print('===================')
@@ -69,23 +75,20 @@ def get_landsat(landsat_number, folder, show_landsat):
     PATH = home + satellite + folder
     os.chdir(PATH)
     
-    (landsat_and_sensor, proccessing_correction_level,
+    (landsat_and_sensor, processing_correction_level,
     wrs_path_row, acquisition_date,
-    proccessing_date, collection_number,
+    processing_date, collection_number,
     collection_category) = folder.split('_')
     
-    if proccessing_correction_level[2] == 'S':
-        PREFIX = folder + '_SR_B'
-    else:
+    if processing_correction_level[1] == '1':
         PREFIX = folder + '_B'
-    
-    if landsat_number == 7:
-        bands = get_l7_bands()
-    elif landsat_number == 8:
-        bands = get_l8_bands()
     else:
-        bands = get_l9_bands()
-
+        if processing_correction_level[2] == 'S':
+            PREFIX = folder + '_SR_B'
+        else:
+            PREFIX = folder + '_B'
+    
+    bands = get_landsat_bands(landsat_number)
     for band in bands:
         file_paths.append(PREFIX + band + '.TIF')
     
@@ -94,8 +97,8 @@ def get_landsat(landsat_number, folder, show_landsat):
     
     width, height = images[1].size
     
-    images, image_arrays, size = compress_images(compression, width, height, images)
-    
+    images, image_arrays, size = compress_image(compression, width, height, images)
+
     time_taken = time.monotonic() - start_time
     print(f'complete! time taken: {round(time_taken, 2)} seconds')
     
@@ -107,17 +110,14 @@ def get_landsat(landsat_number, folder, show_landsat):
     blue, green, nir, swir1, swir2 = image_arrays
     np.seterr(divide='ignore', invalid='ignore')
 
-    minimum = -1 
+    minimum = -1
     maximum = 1
     
     ndwi, mndwi, awei_sh, awei_nsh = get_indices(blue, green, nir, swir1, swir2)
     
     ndwi = np.maximum(np.minimum(ndwi, maximum), minimum) # 'cleaning'
-    
     mndwi = np.maximum(np.minimum(mndwi, maximum), minimum) # 'cleaning'
-    
     awei_sh = np.maximum(np.minimum(awei_sh, maximum), minimum) # 'cleaning'
-    
     awei_nsh = np.maximum(np.minimum(awei_nsh, maximum), minimum) # 'cleaning'
     
     indices = [ndwi, mndwi, awei_sh, awei_nsh]
@@ -126,7 +126,7 @@ def get_landsat(landsat_number, folder, show_landsat):
     print(f'complete! time taken: {round(time_taken, 2)} seconds')
     
     # %%% Showing Images
-    if show_landsat:
+    if do_landsat:
         print('displaying and saving water index images...')
         start_time = time.monotonic()
         plot_image(indices, landsat_number, plot_size, 
@@ -147,7 +147,7 @@ aced with the Mid-Wave Infrared (MIR) band.
 if do_l7:
     l7_indices = get_landsat(landsat_number=7, 
                              folder='LE07_L2SP_201023_20000619_20200918_02_T1', 
-                             show_landsat=do_l7)
+                             do_landsat=do_l7)
 
 """
 Landsat 8 has no Mid-Wave Infrared (MIR) band. This may have effects on calcul-
@@ -156,7 +156,7 @@ ating the Modified Normalised Water Index (MNDWI). (must check)
 if do_l8:
     l8_indices = get_landsat(landsat_number=8, 
                              folder='LC08_L2SP_201024_20241120_20241203_02_T1', 
-                             show_landsat=do_l8)
+                             do_landsat=do_l8)
 
 """
 Landsat 9 has the same band imagers as Landsat 8, meaning that it lacks the MIR
@@ -164,8 +164,8 @@ band.
 """
 if do_l9:
     l9_indices = get_landsat(landsat_number=9, 
-                             folder='LC09_L2SP_201023_20241011_20241012_02_T1', 
-                             show_landsat=do_l9)
+                             folder='LC09_L1TP_201023_20241011_20241011_02_T1', 
+                             do_landsat=do_l9)
 # %% Final
 TOTAL_TIME = time.monotonic() - MAIN_START_TIME
 print(f'total time taken for all processes: {round(TOTAL_TIME, 2)} seconds')
