3ecf0973138c441c455f360645044b4849cc519d
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-02-23 15:55:46 +0000

    Update calculation_functions.py
    moved np.seterr to get_indices function from ipmls

--- calculation_functions.py
+++ calculation_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def get_indices(blue, green, nir, swir1, swir2):
+    np.seterr(divide='ignore', invalid='ignore')
     ndwi = np.where((green + nir) == 0, -1, -(green - nir) / (green + nir))
     mndwi = np.where((green + swir2) == 0, -1, -(green - swir1) / (green + swir1))
     awei_sh = -((blue + (2.5 * green) - (1.5 * (nir + swir1)) - (0.25 * swir2))/(2**16))
