b0cd632f0d55d0669342b74a315f9dcc435f6972
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-02-23 18:34:15 +0000

    Update calculation_functions.py
    fixed calculation of mndwi, awei_sh, and awei_nsh which was just wrong
    no more division by 2**16

--- calculation_functions.py
+++ calculation_functions.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def get_indices(blue, green, nir, swir1, swir2):
     np.seterr(divide='ignore', invalid='ignore')
-    ndwi = np.where((green + nir) == 0, -1, -(green - nir) / (green + nir))
-    mndwi = np.where((green + swir2) == 0, -1, -(green - swir1) / (green + swir1))
-    awei_sh = -((blue + (2.5 * green) - (1.5 * (nir + swir1)) - (0.25 * swir2))/(2**16))
-    awei_nsh = -(((4 * (green - swir1)) - (0.25 * nir - 2.75 * swir2))/(2**16))
+    ndwi = np.where((green + nir) == 0, -1, -(green - nir) / (green + nir)) # FLAG -ve fix
+    mndwi = np.where((green + swir1) == 0, -1, -(green - swir1) / (green + swir1)) # FLAG -ve fix
+    awei_sh =  (blue + 2.5 * green - 1.5 * (nir + swir1) - 0.25 * swir2)
+    awei_nsh = -(4 * (green - swir1) - (0.25 * nir + 2.75 * swir2)) # FLAG -ve fix
     return ndwi, mndwi, awei_sh, awei_nsh
