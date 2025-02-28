3fdef21b7912a9eb1e348a4f22f0f6d6317f79f8
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-02-28 17:08:53 +0000

    Update satellite_functions.py
    added get_sentinel_bands

--- satellite_functions.py
+++ satellite_functions.py
@@ -13,3 +13,12 @@ def get_landsat_bands(landsat_n):
         SWIR1_BAND = '6'
         SWIR2_BAND = '7'
         return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, SWIR2_BAND
+
+def get_sentinel_bands(sentinel_n):
+    if sentinel_n == 2:
+        BLUE_BAND = '2'
+        GREEN_BAND = '3'
+        NIR_BAND = '8'
+        SWIR1_BAND = '11'
+        SWIR2_BAND = '12'
+        return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, SWIR2_BAND
