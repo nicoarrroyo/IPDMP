ac88db2219e5d9486cb3adf8b6c86f406d1e90f1
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-02 18:01:27 +0000

    Update satellite_functions.py
    changed NIR_BAND to 08 from 8A

--- satellite_functions.py
+++ satellite_functions.py
@@ -18,7 +18,7 @@ def get_sentinel_bands(sentinel_n):
     if sentinel_n == 2:
         BLUE_BAND = '02'
         GREEN_BAND = '03'
-        NIR_BAND = '8A'
+        NIR_BAND = '08'
         SWIR1_BAND = '11'
         SWIR2_BAND = '12'
         return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, SWIR2_BAND
