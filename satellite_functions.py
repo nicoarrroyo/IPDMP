03c494c7abc4f69801cbf65aeeba778a65183c41
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-03 13:22:03 +0000

    Update satellite_functions.py
    added high_res check to allow for either 10m, 20m, or 60m

--- satellite_functions.py
+++ satellite_functions.py
@@ -14,11 +14,14 @@ def get_landsat_bands(landsat_n):
         SWIR2_BAND = '7'
         return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, SWIR2_BAND
 
-def get_sentinel_bands(sentinel_n):
+def get_sentinel_bands(sentinel_n, high_res):
     if sentinel_n == 2:
         BLUE_BAND = '02'
         GREEN_BAND = '03'
-        NIR_BAND = '08'
         SWIR1_BAND = '11'
         SWIR2_BAND = '12'
+        if high_res:
+            NIR_BAND = '08'
+        else:
+            NIR_BAND = '8A'
         return BLUE_BAND, GREEN_BAND, NIR_BAND, SWIR1_BAND, SWIR2_BAND
