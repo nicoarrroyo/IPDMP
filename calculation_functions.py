71d516b216f59bc39b2b2a9de6caa21d0a919298
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-03 13:49:06 +0000

    Update calculation_functions.py
    changed get indices to output an array instead of individual indices

--- calculation_functions.py
+++ calculation_functions.py
@@ -6,4 +6,5 @@ def get_indices(blue, green, nir, swir1, swir2):
     mndwi = np.where((green + swir1) == 0, -1, -(green - swir1) / (green + swir1)) # FLAG -ve fix
     awei_sh =  (blue + 2.5 * green - 1.5 * (nir + swir1) - 0.25 * swir2)
     awei_nsh = -(4 * (green - swir1) - (0.25 * nir + 2.75 * swir2)) # FLAG -ve fix
-    return ndwi, mndwi, awei_sh, awei_nsh
+    indices = [ndwi, mndwi, awei_sh, awei_nsh]
+    return indices
