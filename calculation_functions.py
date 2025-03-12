7cd3e1bd869ea73c99b322aed3ade4000fc977ce
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-12 13:59:25 +0000

    Update calculation_functions.py
    calculation_functions
    no longer using np.where(), just let the calculation happen

--- calculation_functions.py
+++ calculation_functions.py
@@ -2,8 +2,9 @@ import numpy as np
 
 def get_indices(blue, green, nir, swir1, swir2):
     np.seterr(divide='ignore', invalid='ignore')
-    ndwi = np.where((green + nir) == 0, -1, -(green - nir) / (green + nir)) # FLAG -ve fix
-    mndwi = np.where((green + swir1) == 0, -1, -(green - swir1) / (green + swir1)) # FLAG -ve fix
+    # maybe the negative has to happen because of how matplotlib works
+    ndwi = -((green - nir) / (green + nir)) # FLAG -ve fix
+    mndwi = -((green - swir1) / (green + swir1)) # FLAG -ve fix
     awei_sh =  (blue + 2.5 * green - 1.5 * (nir + swir1) - 0.25 * swir2)
     awei_nsh = -(4 * (green - swir1) - (0.25 * nir + 2.75 * swir2)) # FLAG -ve fix
     indices = [ndwi, mndwi, awei_sh, awei_nsh]
