541c99ef907b90b030f4db2c8881e98e2938599c
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-12 21:17:10 +0000

    Delete earth_engine_functions.py
    goodbye!

--- earth_engine_functions.py
+++
@@ -1,9 +0,0 @@
-import ee
-
-def authenticate_and_initialise():
-    try:
-        ee.Authenticate()
-        ee.Initialize(project='ee-nicolasrenatoarroyo')
-        print(ee.String('Hello from the Earth Engine servers!').getInfo())
-    except:
-        print('Failed connection to Earth Engine servers')
