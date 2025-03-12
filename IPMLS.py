6c9a4351f4b25aab352df11269b7fb450638c36a
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-12 13:58:40 +0000

    Update IPMLS.py
    increased plot and chunk plot sizes for uni mode
    replaced chunk_plot_size with plot_size_chunk
    moved entire step 5 in the data_labelling conditional
    changed showing indices print output to "saving and displaying" from "displaying and saving"
    changed prefix to not include _B at the end, moved this to file_path assignment
    added a section to find the TCI (true colour image) file in 10m after searching for RGB

--- IPMLS.py
+++ IPMLS.py
@@ -37,17 +37,16 @@ compression = 1 # 1 for full-sized images, bigger integer for smaller images
 dpi = 3000 # 3000 for full resolution, below 1000, images become fuzzy
 n_chunks = 5000 # number of chunks into which images are split
 save_images = False
-high_res = False # use finer 10m spatial resolution (slower)
-# main parent path where all image files are stored
-uni_mode = True
+high_res = True # use finer 10m spatial resolution (slower)
 label_data = True
+uni_mode = True
 if uni_mode:
-    plot_size = (5, 5) # larger plots increase detail and pixel count
-    chunk_plot_size = (7, 5)
+    plot_size = (6, 6) # larger plots increase detail and pixel count
+    plot_size_chunk = (8, 6)
     HOME = "C:\\Users\\c55626na\\OneDrive - The University of Manchester\\Individual Project"
 else:
     plot_size = (3, 3) # larger plots increase detail and pixel count
-    chunk_plot_size = (5, 3)
+    plot_size_chunk = (5, 3)
     HOME = "C:\\Users\\nicol\\Documents\\UoM\\YEAR 3\\Individual Project\\Downloads"
 
 responses = np.zeros((2, n_chunks))
@@ -92,18 +91,18 @@ def get_sat(sat_name, sat_number, folder):
     (sentinel_name, instrument_and_product_level, datatake_start_sensing_time, 
      processing_baseline_number, relative_orbit_number, tile_number_field, 
      product_discriminator_and_format) = folder.split("_")
-    prefix = (f"{tile_number_field}_{datatake_start_sensing_time}_B")
+    prefix = (f"{tile_number_field}_{datatake_start_sensing_time}")
     bands = get_sentinel_bands(sat_number, high_res)
     
     for band in bands:
         if high_res:
             if band == "02" or band == "03" or band == "08":
-                file_paths.append(path_10 + prefix + band + "_10m.jp2")
+                file_paths.append(path_10 + prefix + "_B" + band + "_10m.jp2")
             else:
-                file_paths.append(path_20 + prefix + band + "_20m.jp2")
+                file_paths.append(path_20 + prefix + "_B" + band + "_20m.jp2")
         else:
-            file_paths.append(path_60 + prefix + band + "_60m.jp2")
-    
+            file_paths.append(path_60 + prefix + "_B" + band + "_60m.jp2")
+
     image_arrays, size = compress_image(compression, file_paths)
     
     time_taken = time.monotonic() - start_time
@@ -124,6 +123,7 @@ def get_sat(sat_name, sat_number, folder):
     start_time = time.monotonic()
     
     blue, green, nir, swir1, swir2 = image_arrays
+    globals()["im_arrs"] = image_arrays
     indices = get_indices(blue, green, nir, swir1, swir2)
     
     time_taken = time.monotonic() - start_time
@@ -131,7 +131,7 @@ def get_sat(sat_name, sat_number, folder):
     
     # %%% 4. Showing Indices
     if save_images:
-        print("displaying and saving water index images...")
+        print("saving and displaying water index images...")
     else:
         print("displaying water index images...")
     start_time = time.monotonic()
@@ -140,66 +140,70 @@ def get_sat(sat_name, sat_number, folder):
     time_taken = time.monotonic() - start_time
     print(f"complete! time taken: {round(time_taken, 2)} seconds")
     
-    # %%% 5. Slicing Images
-    print("slicing images...")
-    start_time = time.monotonic()
-    
-    # %%%% 5.1 Searching for and Opening RGB Image
-    path = HOME + satellite + folder
-    found_rgb, full_path = find_rgb_file(path)
-    if found_rgb:
-        print("bright RGB image search successful")
-        print("opening 10m resolution RGB image", end="... ")
-        with Image.open(full_path) as rgb_image:
-            rgb_array = np.array(rgb_image)
+    # %%% 5. Data Labelling
+    if label_data:
+        start_time = time.monotonic()
+        
+        # %%%% 5.1 Searching for, Opening, and Converting RGB Image
+        path = HOME + satellite + folder
+        found_rgb, full_path = find_rgb_file(path)
+        if found_rgb:
+            print("bright RGB image search successful")
+            print("opening 10m resolution RGB image", end="... ")
+            with Image.open(full_path) as rgb_image:
+                rgb_array = np.array(rgb_image)
+            print("complete!")
+        else:
+            print("bright RGB image search failed - "
+                  "generating and saving a new 10m resolution RGB image", end="... ")
+            
+            path = HOME + satellite + folder + "\\GRANULE\\" + subdirs[0] + "\\"
+            os.chdir(path)
+            path_10 = (path + "IMG_DATA\\R10m\\")
+            
+            blue_path = path_10 + prefix + "02_10m.jp2"
+            green_path = path_10 + prefix + "03_10m.jp2"
+            red_path = path_10 + prefix + "04_10m.jp2"
+            
+            rgb_array = get_rgb(blue_path, green_path, red_path, 
+                                save_image=True, res=10, show_image=False)
+            print("complete!")
+        
+        tci_file_name = prefix + "_TCI_10m.jp2"
+        tci_path = f"{path}\\GRANULE\\{subdirs[0]}\\IMG_DATA\\R10m\\"
+        with Image.open(tci_path + tci_file_name) as tci_image:
+            tci_array = np.array(tci_image)
+        # %%%% 5.2 Creating Chunks from Satellite Imagery
+        print("creating", n_chunks, "chunks from satellite imagery", end="... ")
+        index_chunks = []
+        for index in indices:
+            index_chunks.append(split_array(array=index, n_chunks=n_chunks))
+        rgb_chunks = split_array(array=rgb_array, n_chunks=n_chunks)
         print("complete!")
-    else:
-        print("bright RGB image search failed - "
-              "generating and saving a new 10m resolution RGB image", end="... ")
         
-        path = HOME + satellite + folder + "\\GRANULE\\" + subdirs[0] + "\\"
-        os.chdir(path)
-        path_10 = (path + "IMG_DATA\\R10m\\")
+        # %%%% 5.3 Outputting Images for Labelling
+        print("outputting images for labelling", end="... ")
+        index_labels = ["NDWI", "MNDWI", "AWEI-SH", "AWEI-NSH"]
+        break_flag = False
         
-        blue_path = path_10 + prefix + "02_10m.jp2"
-        green_path = path_10 + prefix + "03_10m.jp2"
-        red_path = path_10 + prefix + "04_10m.jp2"
-        
-        rgb_array = get_rgb(blue_path, green_path, red_path, 
-                            save_image=True, res=10, show_image=False)
-        print("complete!")
-    # %%%% 5.2 Creating Chunks from Satellite Imagery
-    print("creating", n_chunks, "chunks from satellite imagery", end="... ")
-    index_chunks = []
-    for index in indices:
-        index_chunks.append(split_array(array=index, n_chunks=n_chunks))
-    rgb_chunks = split_array(array=rgb_array, n_chunks=n_chunks)
-    print("complete!")
-    
-    # %%%% 5.3 Outputting Images for Labelling
-    print("outputting images for labelling", end="... ")
-    index_labels = ["NDWI", "MNDWI", "AWEI-SH", "AWEI-NSH"]
-    break_flag = False
-    
-    for i in range(len(index_chunks[0])):
-        if break_flag:
-            break
-        fig, axes = plt.subplots(1, len(indices), figsize=chunk_plot_size)
-        for count, index_label in enumerate(index_labels):
-            axes[count].imshow(index_chunks[count][i])
-            axes[count].set_title(f"{index_label} Chunk {i}", fontsize=6)
-            axes[count].axis("off")
-        plt.tight_layout()
-        plt.show()
-        
-        plt.figure(figsize=plot_size)
-        plt.title(f"RGB Chunk {i}", fontsize=6)
-        plt.imshow(rgb_chunks[i])
-        plt.axis("off")
-        plt.show()
-        
-        # %%%% 5.4 User Labelling
-        if label_data:
+        for i in range(len(index_chunks[0])):
+            if break_flag:
+                break
+            fig, axes = plt.subplots(1, len(indices), figsize=plot_size_chunk)
+            for count, index_label in enumerate(index_labels):
+                axes[count].imshow(index_chunks[count][i])
+                axes[count].set_title(f"{index_label} Chunk {i}", fontsize=6)
+                axes[count].axis("off")
+            plt.tight_layout()
+            plt.show()
+            
+            plt.figure(figsize=plot_size)
+            plt.title(f"RGB Chunk {i}", fontsize=6)
+            plt.imshow(rgb_chunks[i])
+            plt.axis("off")
+            plt.show()
+            
+            # %%%% 5.4 User Labelling
             global response_time
             response_time_start = time.monotonic()
             n_reservoirs = input("how many reservoirs? ")
@@ -218,9 +222,9 @@ def get_sat(sat_name, sat_number, folder):
                         break
                     print("error: non-integer response. type 'break' to save and quit")
                     n_reservoirs = input("how many reservoirs? ")
-        else:
-            print("not labelling data")
-            return indices
+    else:
+        print("not labelling data")
+        return indices
     
     time_taken = time.monotonic() - start_time - response_time
     print(f"complete! time taken: {round(time_taken, 2)} seconds")
