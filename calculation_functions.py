917bcaf11a3a46c625716effa219953ce6f944f5
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-18 10:49:30 +0000

    Update calculation_functions.py
    fixed awei_sh by swapping green and blue (doesn't make much difference)

--- calculation_functions.py
+++ calculation_functions.py
@@ -5,7 +5,7 @@ def get_indices(blue, green, nir, swir1, swir2):
     # maybe the negative has to happen because of how matplotlib works
     ndwi = -((green - nir) / (green + nir)) # FLAG -ve fix
     mndwi = -((green - swir1) / (green + swir1)) # FLAG -ve fix
-    awei_sh =  (blue + 2.5 * green - 1.5 * (nir + swir1) - 0.25 * swir2)
+    awei_sh = (green + 2.5 * blue - 1.5 * (nir + swir1) - 0.25 * swir2)
     awei_nsh = -(4 * (green - swir1) - (0.25 * nir + 2.75 * swir2)) # FLAG -ve fix
     indices = [ndwi, mndwi, awei_sh, awei_nsh]
     return indices
