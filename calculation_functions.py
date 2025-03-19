7bafb28df0d27882d834cc892f2455b24141a408
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-19 13:50:47 +0000

    Update calculation_functions.py
    removed minus signs. it messes with the plots but all the values are negative otherwise
    removed np.seterr

--- calculation_functions.py
+++ calculation_functions.py
@@ -1,11 +1,9 @@
-import numpy as np
 
 def get_indices(blue, green, nir, swir1, swir2):
-    np.seterr(divide='ignore', invalid='ignore')
     # maybe the negative has to happen because of how matplotlib works
-    ndwi = -((green - nir) / (green + nir)) # FLAG -ve fix
-    mndwi = -((green - swir1) / (green + swir1)) # FLAG -ve fix
+    ndwi = ((green - nir) / (green + nir)) # FLAG reciprocal fix
+    mndwi = ((green - swir1) / (green + swir1)) # FLAG reciprocal fix
     awei_sh = (green + 2.5 * blue - 1.5 * (nir + swir1) - 0.25 * swir2)
-    awei_nsh = -(4 * (green - swir1) - (0.25 * nir + 2.75 * swir2)) # FLAG -ve fix
+    awei_nsh = (4 * (green - swir1) - (0.25 * nir + 2.75 * swir2))
     indices = [ndwi, mndwi, awei_sh, awei_nsh]
     return indices
