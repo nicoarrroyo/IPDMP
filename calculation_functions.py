0be5982aed70b5e98efc76c31df77c97036b2103
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-20 13:44:53 +0000

    Update calculation_functions.py
    added np.seterr back to fix the clouds = 0 from image functions

--- calculation_functions.py
+++ calculation_functions.py
@@ -1,8 +1,10 @@
+import numpy as np
 
 def get_indices(blue, green, nir, swir1, swir2):
+    np.seterr(divide="ignore", invalid="ignore")
     # maybe the negative has to happen because of how matplotlib works
-    ndwi = ((green - nir) / (green + nir)) # FLAG reciprocal fix
-    mndwi = ((green - swir1) / (green + swir1)) # FLAG reciprocal fix
+    ndwi = ((green - nir) / (green + nir)) # FLAG bad values
+    mndwi = ((green - swir1) / (green + swir1)) # FLAG bad values
     awei_sh = (green + 2.5 * blue - 1.5 * (nir + swir1) - 0.25 * swir2)
     awei_nsh = (4 * (green - swir1) - (0.25 * nir + 2.75 * swir2))
     indices = [ndwi, mndwi, awei_sh, awei_nsh]
