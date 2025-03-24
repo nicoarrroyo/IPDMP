fdaf7b21b8d2250308a47e77e6e777a4e83f6af4
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-24 19:12:51 +0000

    Update calculation_functions.py
    added prints to update the user on progress

--- calculation_functions.py
+++ calculation_functions.py
@@ -4,8 +4,13 @@ def get_indices(blue, green, nir, swir1, swir2):
     np.seterr(divide="ignore", invalid="ignore")
     
     ndwi = ((green - nir) / (green + nir)) # FLAG bad values
+    print(".", end="")
     mndwi = ((green - swir1) / (green + swir1)) # FLAG bad values
+    print(".", end="")
     awei_sh = (green + 2.5 * blue - 1.5 * (nir + swir1) - 0.25 * swir2)
+    print(".", end="")
     awei_nsh = (4 * (green - swir1) - (0.25 * nir + 2.75 * swir2))
+    print(".", end="")
     indices = [ndwi, mndwi, awei_sh, awei_nsh]
+    print(" complete!")
     return indices
