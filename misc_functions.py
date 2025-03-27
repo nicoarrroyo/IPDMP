1774f8e5d4bd5846260c0a5ea470775ec6eadbf8
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-27 15:43:48 +0000

    Update misc_functions.py
    added a function called check_file_permission which checks if the file is open to prevent "no permission" crashes
    this function is called several times in ipdgs as well as in blank_entry_check
    also got rid of performance estimator

--- misc_functions.py
+++ misc_functions.py
@@ -22,34 +22,6 @@ def table_print(**kwargs):
         print(f"| {key.ljust(max_var_length)} | {str(value).ljust(max_value_length)} |")
     print(separator)
 
-def performance_estimate(gee_connect, compression, dpi, plot_size, save_images, 
-                         high_res_Sentinel, do_l7, do_l8, do_l9, do_s2):
-    perf_params = [gee_connect, compression, dpi, plot_size, save_images,
-                   high_res_Sentinel, do_l7, do_l8, do_l9, do_s2]
-    
-    perf_counter = 0
-    for param in perf_params:
-        if isinstance(param, bool) and param:
-            perf_counter += 1
-            if do_s2 and param == high_res_Sentinel:
-                perf_counter += 4
-            if param == save_images:
-                perf_counter += 3
-        
-        if isinstance(param, int) and not isinstance(param, bool):
-            if param == compression and param <= 3:
-                perf_counter += 3
-            else:
-                perf_counter += 1
-            if param == dpi and param >= 2000 and save_images:
-                perf_counter += 1
-        if isinstance(param, tuple):
-            if param == plot_size and param > (3, 3):
-                perf_counter += 2
-    
-    max_counter = 30 # incorrect - needs tweaking
-    return perf_counter/max_counter
-
 def split_array(array, n_chunks):
     rows = np.array_split(array, np.sqrt(n_chunks), axis=0) # split into rows
     split_arrays = [np.array_split(row_chunk, np.sqrt(n_chunks), 
@@ -66,6 +38,7 @@ def rewrite(write_file, rows):
         write_file.write(f"{entry}\n")
 
 def blank_entry_check(file):
+    check_file_permission(file_name=file)
     print("checking for blank entries...", end="... ")
     
     cleaned_rows = []
@@ -86,6 +59,15 @@ def blank_entry_check(file):
             csv_writer.writerows(cleaned_rows)
         print(f"complete! {len(invalid_rows)} invalid entries were removed on ", 
               invalid_rows)
+
+def check_file_permission(file_name):
+    while True:
+        try: # check if file is open
+            with open(file_name, mode="a"):
+                break
+        except IOError:
+            print("could not open file - please close the responses file")
+            input("press enter to retry")
 
 import sys
 import time
