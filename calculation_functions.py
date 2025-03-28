453a37f60ad56ea26bd60694d6784612043be8e5
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-28 15:10:48 +0000

    INDEX CALCULATION FIXED !!!!!! calculation_functions.py
    removed # flag bad values (because they're not bad anymore!!)
    the fix was in IPDGS :)

--- calculation_functions.py
+++ calculation_functions.py
@@ -3,9 +3,9 @@ import numpy as np
 def get_indices(blue, green, nir, swir1, swir2):
     np.seterr(divide="ignore", invalid="ignore")
     
-    ndwi = ((green - nir) / (green + nir)) # FLAG bad values
+    ndwi = ((green - nir) / (green + nir))
     print(".", end="")
-    mndwi = ((green - swir1) / (green + swir1)) # FLAG bad values
+    mndwi = ((green - swir1) / (green + swir1))
     print(".", end="")
     awei_sh = (green + 2.5 * blue - 1.5 * (nir + swir1) - 0.25 * swir2)
     print(".", end="")
