6c5b30cf1cf615c5de7576fd99b31c541926caa9
nico <162636059+nicoarrroyo@users.noreply.github.com>
2025-03-28 16:58:21 +0000

    Update calculation_functions.py
    removed all print(".") update prints

--- calculation_functions.py
+++ calculation_functions.py
@@ -4,13 +4,8 @@ def get_indices(blue, green, nir, swir1, swir2):
     np.seterr(divide="ignore", invalid="ignore")
     
     ndwi = ((green - nir) / (green + nir))
-    print(".", end="")
     mndwi = ((green - swir1) / (green + swir1))
-    print(".", end="")
     awei_sh = (green + 2.5 * blue - 1.5 * (nir + swir1) - 0.25 * swir2)
-    print(".", end="")
     awei_nsh = (4 * (green - swir1) - (0.25 * nir + 2.75 * swir2))
-    print(".", end="")
     indices = [ndwi, mndwi, awei_sh, awei_nsh]
-    print(" complete!")
     return indices
